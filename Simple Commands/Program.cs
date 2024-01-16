1ce69559aa4d4f244eafead7e0add44d337cdd73
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-16 17:21:25 +0200

    Add files via upload

---
+++ Simple Commands/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace nameAndExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Miglena");
+            Console.WriteLine(5 + 3);
+        }
+    }
+}
