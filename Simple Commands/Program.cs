d9c5a0c384789546c6849ee41b56efe31964d5bb
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-18 18:09:32 +0200

    Add files via upload

---
+++ Simple Commands/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace squareOf7x7Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine("* * * * * * *");
+            }
+            
+        }
+    }
+}
