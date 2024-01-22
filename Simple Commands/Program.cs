261329a954833df52c5e446214d52bc8c0cdc15d
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-22 18:15:43 +0200

    Add files via upload

---
+++ Simple Commands/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace customerInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string email = Console.ReadLine();
+
+            Console.WriteLine($"Customer: {firstName} {lastName} ({email})");
+        }
+    }
+}
