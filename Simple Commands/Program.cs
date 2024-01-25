c66a99eba11a3bf9745aa09e8548c3d0b54f0edd
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-25 22:04:57 +0200

    Add files via upload

---
+++ Simple Commands/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace squareArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int area = length * length;
+            Console.WriteLine(area);
+        }
+    }
+}
