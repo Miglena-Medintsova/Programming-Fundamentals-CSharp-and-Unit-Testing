75042fe4f0fbd2e2d548cc7bcb39402ffca08154
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-26 23:29:46 +0200

    Add files via upload

---
+++ Simple Commands/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace rectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int area = length * width;
+
+            Console.WriteLine(area);
+        }
+    }
+}
