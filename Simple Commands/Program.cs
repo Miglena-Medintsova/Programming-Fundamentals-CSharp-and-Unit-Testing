bbcef3f256e8129f77fb045442d6497ccbea5a75
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-29 23:20:51 +0200

    Add files via upload

---
+++ Simple Commands/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace trianglePerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int side1 = int.Parse(Console.ReadLine());
+            int side2 = int.Parse(Console.ReadLine());
+            int side3 = int.Parse(Console.ReadLine());
+
+            int perimeter = side1 + side2 + side3;
+
+            Console.WriteLine(perimeter);
+        }
+    }
+}
