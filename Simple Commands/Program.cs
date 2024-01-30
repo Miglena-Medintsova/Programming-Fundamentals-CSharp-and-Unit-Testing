d48e9d9431bc2f7ab90857b734336731c6d802b8
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-30 23:19:57 +0200

    Add files via upload

---
+++ Simple Commands/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace inchesToCmConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inches = int.Parse(Console.ReadLine());
+            double cm = inches * 2.54;
+            Console.WriteLine(cm);
+        }
+    }
+}
