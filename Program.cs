180f20b0d4e025ccaca84ba98c6e6e7cc9ffa22e
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-01-31 23:11:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int days = int.Parse(Console.ReadLine());
+int minutes = days * 24 * 60;
+Console.WriteLine($"Minutes = {minutes}");
