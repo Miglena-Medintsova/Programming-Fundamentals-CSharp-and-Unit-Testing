94ca12105ab37fe27ee7dc135e7168e069e3dda5
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-05 23:24:45 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+double dogFood = double.Parse(Console.ReadLine());
+double catFood = double.Parse(Console.ReadLine());
+
+double dogFoodPrice = dogFood * 2.5;
+double catFoodPrice = catFood * 4;
+
+double expenses = dogFoodPrice + catFoodPrice;
+
+Console.WriteLine($"{expenses:F2} lv.");
+
+
+
