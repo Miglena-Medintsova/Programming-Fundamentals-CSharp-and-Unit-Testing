a77e7bf4a0ab4a37372fe1d1eb1742f08d8e0219
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-06 23:21:39 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = Console.ReadLine();
+int projects = int.Parse(Console.ReadLine());
+
+int neededHours = projects * 3;
+
+Console.WriteLine($"The architect {name} will need {neededHours} hours to complete {projects} project/s.");
+
