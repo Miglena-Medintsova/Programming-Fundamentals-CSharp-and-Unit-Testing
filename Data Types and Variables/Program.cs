03629a571a112a44c724800a149d18335145e0a3
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-10 23:56:33 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿double firstNumber = double.Parse(Console.ReadLine());
+double secondNumber = double.Parse(Console.ReadLine());
+
+double addingResult = firstNumber + secondNumber;
+double substractionResult = firstNumber - secondNumber;
+double multiplicationResult = firstNumber * secondNumber;
+double divisionResult = firstNumber / secondNumber;
+
+Console.WriteLine($"{firstNumber:F2} + {secondNumber:F2} = {addingResult:F2}");
+Console.WriteLine($"{firstNumber:F2} - {secondNumber:F2} = {substractionResult:F2}");
+Console.WriteLine($"{firstNumber:F2} * {secondNumber:F2} = {multiplicationResult:F2}");
+Console.WriteLine($"{firstNumber:F2} / {secondNumber:F2} = {divisionResult:F2}"); 
