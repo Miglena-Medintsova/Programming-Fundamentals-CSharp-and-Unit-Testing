8f2e2fc8bfba225d41bdb1576f092c45129e7918
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-12 22:50:58 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿double bathroomWidth = double.Parse(Console.ReadLine());
+double bathroomHeight = double.Parse(Console.ReadLine());
+double tileWidth = double.Parse(Console.ReadLine());
+double tileHeight = double.Parse(Console.ReadLine());
+
+double bathroomArea = bathroomWidth * bathroomHeight;
+double surplus = bathroomArea * 1.1;
+double tileArea = tileWidth * tileHeight;
+double tilesNeeded = surplus / tileArea;
+
+Console.WriteLine(Math.Round(tilesNeeded));
+
+
