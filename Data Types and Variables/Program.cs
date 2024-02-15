1af4ec11e10da35efa6c9c75797f25e0f9e8fcd2
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-15 13:58:38 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿double depositedAmount = double.Parse(Console.ReadLine());
+double termOfTheDeposit = double.Parse(Console.ReadLine());
+double annualInterestRate = double.Parse(Console.ReadLine());
+
+double accumulatedInterest = depositedAmount * (annualInterestRate / 100);
+double interestForMonth = accumulatedInterest / 12;
+double totalAmount = depositedAmount + termOfTheDeposit * interestForMonth;
+
+Console.WriteLine(totalAmount);
+
