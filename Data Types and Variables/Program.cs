08d944a3f7525aed5d987d8558329357b1b7bdb6
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-17 16:37:52 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,8 @@
+﻿int numberOfPages = int.Parse(Console.ReadLine());
+int readedPagesInHour = int.Parse(Console.ReadLine());
+int numberOfDaysNeeded = int.Parse(Console.ReadLine());
+
+int totalReadingTime = numberOfPages / readedPagesInHour;
+int hourPerDayNeeded = totalReadingTime / numberOfDaysNeeded;
+
+Console.WriteLine(hourPerDayNeeded);
