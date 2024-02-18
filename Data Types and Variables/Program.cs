8e5d84508468c7e6c3249dc1bda468d2d7afd55b
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-18 11:18:13 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿double packagesOfPens = double.Parse(Console.ReadLine());
+double packagesOfMarkers = double.Parse(Console.ReadLine());
+double litersOfBoardCleaner = double.Parse(Console.ReadLine());
+double discountPercentage = double.Parse(Console.ReadLine());
+
+double priceOfPens = packagesOfPens * 5.8;
+double priceOfMarkers = packagesOfMarkers * 7.2;
+double priceOfBoardCleaner = litersOfBoardCleaner * 1.2;
+double priceForAllMaterials = priceOfPens + priceOfMarkers + priceOfBoardCleaner;
+double discount = discountPercentage / 100;
+double priceAfterDiscount = priceForAllMaterials - (priceForAllMaterials * discount);
+
+Console.WriteLine(priceAfterDiscount);
