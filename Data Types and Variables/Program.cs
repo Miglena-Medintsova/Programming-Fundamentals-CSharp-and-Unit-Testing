19ecf253516c30e9e634d1f03a7f05f16c322476
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-22 16:01:37 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿int nylon = int.Parse(Console.ReadLine());
+int paint = int.Parse(Console.ReadLine());
+int thinner = int.Parse(Console.ReadLine());
+int hoursNeeded = int.Parse(Console.ReadLine());
+
+double nylonAmount = (nylon + 2) * 1.5;
+double paintAmount = (paint * 1.1) * 14.5;
+double thinnerAmount = thinner * 5;
+double totalAmount = nylonAmount + paintAmount + thinnerAmount + 0.4;
+double craftmenAmount = (totalAmount * 0.3) * hoursNeeded;
+double sum = totalAmount + craftmenAmount;
+
+Console.WriteLine(sum);
+
+
+
+
