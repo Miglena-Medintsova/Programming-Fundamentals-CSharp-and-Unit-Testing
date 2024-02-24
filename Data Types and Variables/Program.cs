b39c9f07ef4389a6163e194c26113cace137ab4a
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-24 10:54:55 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿int chickenMenu = int.Parse(Console.ReadLine());
+int fishMenu = int.Parse(Console.ReadLine());
+int vegetarianMenu = int.Parse(Console.ReadLine());
+
+double chickenMenuPrice = chickenMenu * 10.35;
+double fishMenuPrice = fishMenu * 12.4;
+double vegetarianMenuPrice = vegetarianMenu * 8.15;
+double totalCost = chickenMenuPrice + fishMenuPrice + vegetarianMenuPrice;
+double dessert = 0.2 * totalCost;
+double totalOrderPrice = totalCost + dessert + 2.5;
+
+Console.WriteLine(totalOrderPrice);
