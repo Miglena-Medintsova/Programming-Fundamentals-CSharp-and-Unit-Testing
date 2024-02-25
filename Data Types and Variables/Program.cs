0227bcb864d2bfbce6e6251635a9451286f4b374
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-25 18:44:27 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿int annualTrainingFee = int.Parse(Console.ReadLine());
+
+double sneakers = annualTrainingFee * 0.6;
+double team = sneakers * 0.8;
+double basketball = 0.25 * team;
+double accessories = 0.2 * basketball;
+double totalPrice = annualTrainingFee + sneakers + team + basketball + accessories;
+
+Console.WriteLine(totalPrice);
