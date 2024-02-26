2366bbeb8de56a37de6f4e0681cc803be8b03d6f
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-02-26 12:50:24 +0200

    Add files via upload

---
+++ Data Types and Variables/Program.cs
@@ -0,0 +1,11 @@
+﻿int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percentage = double.Parse(Console.ReadLine());
+
+double allVolume = length * width * height;
+double volumeLiters = allVolume * 0.001;
+double occupiedSpace = percentage / 100;
+double requiredLiters = volumeLiters * (1 - occupiedSpace);
+
+Console.WriteLine($"{requiredLiters:F2}");
