744692f2d964a243da4c20ca3baf56ce791f202e
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-03-05 10:53:52 +0200

    Add files via upload

---
+++ Simple Conditional Statements/Program.cs
@@ -0,0 +1,14 @@
+﻿string ticketType = Console.ReadLine();
+
+if (ticketType == "student")
+{
+    Console.WriteLine($"${1:F2}");
+}
+else if (ticketType == "regular")
+{
+    Console.WriteLine($"${1.60:F2}");
+}
+else
+{
+    Console.WriteLine("Invalid ticket type!");
+}
