48c0f5e68db590370076a913f11a1493643a9bdb
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-03-06 23:09:30 +0200

    Add files via upload

---
+++ Complex Conditional Statements/Program.cs
@@ -0,0 +1,24 @@
+﻿string typeOfMovie = Console.ReadLine();
+int countOfRows = int.Parse(Console.ReadLine());
+int countOfSeats = int.Parse(Console.ReadLine());
+
+double totalCount = countOfRows * countOfSeats;
+double totalPrice = 0;
+
+switch (typeOfMovie)
+   {
+    case "Premiere":
+        totalPrice = totalCount * 12;
+        break;
+
+    case "Normal":
+        totalPrice = totalCount * 7.5;
+
+        break;
+
+    case "Discount":
+        totalPrice = totalCount * 5;
+        break;
+}
+Console.WriteLine($"{totalPrice:F2}");
+
