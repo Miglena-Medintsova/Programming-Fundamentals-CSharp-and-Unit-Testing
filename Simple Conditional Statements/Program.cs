07247426e270c1678660d57177e43d0be73348bf
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-03-06 22:36:44 +0200

    Add files via upload

---
+++ Simple Conditional Statements/Program.cs
@@ -0,0 +1,29 @@
+﻿string typeOfFigure = Console.ReadLine();
+
+
+if (typeOfFigure == "square")
+{
+    double side = double.Parse(Console.ReadLine());
+
+    double areaOfSquare = side * side;
+
+    Console.WriteLine($"{areaOfSquare:F2}");
+}
+else if (typeOfFigure == "rectangle")
+{
+    double width = double.Parse(Console.ReadLine());
+    double length = double.Parse(Console.ReadLine());
+
+    double areaOfRectangle = width * length;
+
+    Console.WriteLine($"{areaOfRectangle:F2}");
+}
+else if (typeOfFigure == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+
+    double areaOfCircle = Math.PI * radius * radius;
+
+    Console.WriteLine($"{areaOfCircle:F2}");
+}
+
