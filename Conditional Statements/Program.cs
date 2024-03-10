f0b0ab1f5bad3a8b25142069617e8f9e59c8ec00
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-03-10 23:15:44 +0200

    Add files via upload

---
+++ Conditional Statements/Program.cs
@@ -0,0 +1,35 @@
+﻿double numberOne = double.Parse(Console.ReadLine());
+double numberTwo = double.Parse(Console.ReadLine());
+double numberThree = double.Parse(Console.ReadLine());
+
+double sum = 0;
+
+if (numberOne < 0)
+{
+    sum++;
+}
+if (numberTwo < 0)
+{
+    sum++;
+}
+if (numberThree < 0)
+{
+    sum++;
+}
+
+if (sum == 1)
+{
+    Console.WriteLine("negative");
+}
+else if (sum == 2)
+{
+    Console.WriteLine("positive");
+}
+else if (sum == 3)
+{
+    Console.WriteLine("negative");
+}
+else if (sum == 0)
+{
+    Console.WriteLine("zero");
+}
