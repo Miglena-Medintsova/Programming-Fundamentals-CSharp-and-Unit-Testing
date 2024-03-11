f754ae267437f46dae8f7e5e305ee8b9a4e820c8
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-03-11 23:45:44 +0200

    Add files via upload

---
+++ Conditional Statements/Program.cs
@@ -0,0 +1,11 @@
+﻿int hourOfTheDay = int.Parse(Console.ReadLine());
+string dayOfTheWeek = Console.ReadLine();
+
+if (hourOfTheDay >= 10 && hourOfTheDay <= 18 && dayOfTheWeek != "Sunday")
+{
+    Console.WriteLine("open");
+}
+else
+{
+    Console.WriteLine("closed");
+}
