cf171db5758946c0d2672fa7d6018c4b98e5dbc8
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-03-13 18:56:26 +0200

    Add files via upload

---
+++ Conditional Statements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+using System.Xml.Schema;
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+double minutes = num1 + num2 + num3;
+double totalMins = minutes / 60;
+double seconds = minutes % 60;
+
+if (seconds <= 9)
+{
+    Console.WriteLine($"{Math.Floor(totalMins)}:0{seconds}");
+}
+else
+{
+    Console.WriteLine($"{Math.Floor(totalMins)}:{seconds}");
+}
+
+
+
+
+
