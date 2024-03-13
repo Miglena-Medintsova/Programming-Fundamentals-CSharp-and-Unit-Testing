c5a782899f6f76526baae3b77eff89a6f00ddcd7
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-03-13 19:00:57 +0200

    Add files via upload

---
+++ For & While Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write(i);
+
+    sum = sum + i;
+
+    if (i < num)
+    {
+        Console.Write("+");
+    }
+}
+Console.Write($"={sum}");
