4c88300e4b30ac8cfaca849dc9b6420f7919bab5
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-03-14 20:27:23 +0200

    Add files via upload

---
+++ For & While Loops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int countOfNumbers = int.Parse(Console.ReadLine());
+
+double sum = 0;
+
+for (int i = 1; i <= countOfNumbers; i++)
+{
+    double numbers = double.Parse(Console.ReadLine());
+    sum = sum + numbers;
+}
+Console.WriteLine(sum);
