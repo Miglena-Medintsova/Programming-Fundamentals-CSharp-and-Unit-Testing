9c190bff1251e3ad8771279d2bdd9060e440cd9a
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-03-17 23:57:49 +0200

    Add files via upload

---
+++ For & While Loops/Program.cs
@@ -0,0 +1,9 @@
+﻿int d = int.Parse(Console.ReadLine());
+int currentDay = 0;
+
+for (int i = d; i >= 1; i--)
+{
+    Console.WriteLine($"{i} days before the exam");
+}
+Console.WriteLine("The exam has come");
+
