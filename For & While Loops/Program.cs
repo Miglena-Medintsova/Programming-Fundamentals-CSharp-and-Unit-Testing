8e481e7462508517f5c3a32dae8af22966663b54
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-03-29 12:39:19 +0200

    Add files via upload

---
+++ For & While Loops/Program.cs
@@ -0,0 +1,18 @@
+﻿int amountOfNums = int.Parse(Console.ReadLine());
+int maxNumber = int.MinValue;
+int sum = 0;
+
+for (int i = 0; i < amountOfNums; i++)
+{
+
+    int currentNumber = int.Parse((Console.ReadLine()));
+    sum = 0;
+
+    if (currentNumber > maxNumber)
+    {
+        sum += currentNumber;
+        maxNumber = currentNumber;
+    }
+}
+Console.WriteLine(maxNumber);
+
