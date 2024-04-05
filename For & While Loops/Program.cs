a7397734958e4e0aa216b4aa80cf9e13dbaada68
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-04-05 23:46:46 +0300

    Add files via upload

---
+++ For & While Loops/Program.cs
@@ -0,0 +1,30 @@
+﻿int number = int.Parse(Console.ReadLine());
+int tempNumber = number;
+bool isSpecial = true;
+
+while (tempNumber > 0)
+{
+    int currentLastDigit = tempNumber % 10;
+    tempNumber = tempNumber / 10;
+
+    if (number % currentLastDigit != 0)
+    {
+
+        isSpecial = false;
+        break;
+    }
+}
+if (isSpecial)
+{
+    Console.WriteLine($"{number} is special");
+}
+else
+{
+    Console.WriteLine($"{number} is not special");
+}
+
+
+
+
+
+
