f7f2636b512222662110f1e889f150db13cdb593
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-04-07 12:30:53 +0300

    Add files via upload

---
+++ For & While Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿int stopNumber = int.Parse(Console.ReadLine());
+int currentNumber = int.Parse(Console.ReadLine());
+double sum = 0;
+
+while (currentNumber >= stopNumber || currentNumber <= stopNumber)
+{
+    sum = 0;
+    sum = currentNumber + (currentNumber * 0.2) ;
+    currentNumber = int.Parse(Console.ReadLine());
+    
+    if (currentNumber == stopNumber)
+    {
+        break;
+    }
+}
+Console.WriteLine(sum);
+
