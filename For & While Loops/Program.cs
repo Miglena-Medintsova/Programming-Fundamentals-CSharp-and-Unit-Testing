4501f98d01e1da949bfb3d1993098e6745b49675
Miglena Medintsova <142172186+Miglena-Medintsova@users.noreply.github.com>
2024-04-07 23:21:59 +0300

    Add files via upload

---
+++ For & While Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿string input = Console.ReadLine();
+double balance = 0;
+
+while (input != "End")
+{
+    double money = double.Parse(input); 
+
+    if (money > 0)
+    {
+        balance += money;
+        Console.WriteLine($"Increase: {money}");
+    }
+    else
+    {
+        balance -= Math.Abs(money);
+        Console.WriteLine($"Decrease: {Math.Abs(money)}");
+    }
+    input = Console.ReadLine();
+}
+Console.WriteLine($"Balance: {balance}");
+
+
+
+
+
